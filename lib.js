eb33b397b954826162cc8c179d25964febcc5518
Stavros <st.zavrakas@gmail.com>
2019-11-13 16:54:42 +0000

    Fix indentation and spaces

--- lib.js
+++ lib.js
@@ -1,24 +1,17 @@
 import { createSelector } from 'reselect';
 
-// const { createSelector } = require('reselect');
-
 const selectActivity = state => state.activity;
 
 export const selectRoundData = createSelector(
-    selectActivity,
-    activity => {
-        const { round_index, questions } = activity;
+  selectActivity,
+  activity => {
+    const { round_index, questions } = activity;
 
-        const { round_title, round_type } = questions[round_index];
+    const { round_title, round_type } = questions[round_index];
 
-        return {
-
-            round_title,
-
-            round_type
-
-        };
-    }
+    return {
+      round_title,
+      round_type
+    };
+  }
 );
-
-// module.exports = selectRoundData;
